b0c59dfc0d313a625eea4ef57b9471c58654edfb
ahew <Deniskazberuk@hotmail.com>
2024-02-16 23:01:19 +0100

    GUI + load button work for v1

---
+++ main.py
@@ -0,0 +1,6 @@
+from window import MyWidget
+
+if __name__ == "__main__":
+    app = MyWidget()
+    app.show()
+    app.exec()
--- window.py
+++ window.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from image import Image
 from pixel import Pixel
 from encoding import Encoder, Decoder
@@ -13,13 +14,13 @@ class MyWidget(QtWidgets.QWidget):
         self.layout = QtWidgets.QVBoxLayout()
         self.setWindowTitle('Projet informatique avec 17 pages de consignes')
         self.is_image = False
+        self.load_button = QtWidgets.QPushButton('Load')
+        self.save_button = QtWidgets.QPushButton('Save')
 
         self.init_UI()
 
     def init_UI(self):
-        self.load_button = QtWidgets.QPushButton('Load')
         self.load_button.clicked.connect(self.load)
-        self.save_button = QtWidgets.QPushButton('Save')
         self.save_button.setEnabled(self.is_image)
 
         but_box = QtWidgets.QHBoxLayout()
@@ -37,7 +38,11 @@ class MyWidget(QtWidgets.QWidget):
         self.show()
     @QtCore.Slot()
     def load(self):
-        photo = Decoder.load_from('./imgs/checkers1.ulbmp')
+        photo = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', os.getcwd(), 'Images (*.ulbmp *.ULBMP)')[0]
+        try:
+            photo = Decoder.load_from(photo)
+        except Exception as e:
+            QtWidgets.QErrorMessage(self).showMessage(str(e))
         self.is_image = True
         self.image = QtGui.QImage(photo.width, photo.height, QtGui.QImage.Format_RGB32)
         for i in range(photo.width):
@@ -49,6 +54,7 @@ class MyWidget(QtWidgets.QWidget):
 
         pix = QtGui.QPixmap(self.image)
         self.label.setPixmap(pix)
+        self.label.setMargin(10)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.save_button.setEnabled(self.is_image)
         self.layout.addWidget(self.label)
