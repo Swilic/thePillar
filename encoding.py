8b4b873a4ae32d8c3611ebfc44fed57d3b6171bd
ahew <Deniskazberuk@hotmail.com>
2024-02-19 21:40:49 +0100

    300 blocks test passed

--- encoding.py
+++ encoding.py
@@ -16,6 +16,12 @@ def save_v1(f, img: 'Image') -> None:
 
 
 def save_v2(f, img: 'Image') -> None:
+    def write(file, c: int, pix: 'Pixel'):
+        print(c)
+        file.write(c.to_bytes(length=1, byteorder='big', signed=False))
+        for i in range(3):
+            file.write(pix.color[i].to_bytes(length=1, byteorder='big', signed=False))
+
     header = ULBMP + b'\x02\x0c\x00'
     width = img.width.to_bytes(length=2, byteorder='little', signed=False)
     height = img.height.to_bytes(length=2, byteorder='little', signed=False)
@@ -25,29 +31,35 @@ def save_v2(f, img: 'Image') -> None:
         if img[i] == img[i-1]:
             count += 1
         else:
-            f.write(count.to_bytes(length=1, byteorder='little', signed=False))
-            [f.write(j.to_bytes(length=1, byteorder='little', signed=False)) for j in img[i-1].color]
+            write(f, count, img[i-1])
             count = 1
-    f.write(count.to_bytes(length=1, byteorder='little', signed=False))
-    [f.write(j.to_bytes(length=1, byteorder='little', signed=False)) for j in img[i - 1].color]
+        if count == 255:
+            write(f, 255, img[i-1])
+            count = 0
+    write(f, count, img[-1])
 
 
 class Encoder:
-    def __init__(self, img: 'Image'):
+    def __init__(self, img: 'Image', version: int = 1):
         self.__image = img
+        self.__version = version
 
-    @property
-    def image(self) -> 'Image':
-        return self.__image
-
-    def save_to(self, path: str, v=1) -> None:
+    def save_to(self, path: str) -> None:
         case = {
             1: save_v1,
             2: save_v2,
         }
         with open(path, 'wb') as f:
-            if 1 <= v <= 2:
-                case[v](f, self.image)
+            if 1 <= self.version <= 2:
+                case[self.version](f, self.image)
+
+    @property
+    def image(self) -> 'Image':
+        return self.__image
+
+    @property
+    def version(self) -> int:
+        return self.__version
 
 
 def verify_header(header: bytes) -> bool:
@@ -63,7 +75,7 @@ def verify_header(header: bytes) -> bool:
     return True
 
 
-def load_v1(file_list: list) -> list[Pixel]:
+def load_v1(file_list: list) -> list['Pixel']:
     pixel_list = list()
     for i in range(0, len(file_list), 3):
         pixel_list.append(Pixel(file_list[i], file_list[i + 1], file_list[i + 2]))
@@ -71,20 +83,20 @@ def load_v1(file_list: list) -> list[Pixel]:
     return pixel_list
 
 
-def load_v2(file_list: list) -> list[Pixel]:
+def load_v2(file_list: list) -> list['Pixel']:
     pixel_list = list()
     for i in range(0, len(file_list), 4):
-        for j in range(int.from_bytes(file_list[i:i+1], byteorder='little', signed=False)):
+        for j in range(int.from_bytes(file_list[i:i+1], byteorder='big', signed=False)):
             pixel_list.append(Pixel(file_list[i+1], file_list[i+2], file_list[i+3]))
 
     return pixel_list
 
 
 def get_header_info(header: bytes) -> tuple[int, int, int]:
-    x = int.from_bytes(header[5:6], byteorder='little', signed=False)
-    y = int.from_bytes(header[8:10], byteorder='little', signed=False)
-    z = int.from_bytes(header[10:12], byteorder='little', signed=False)
-    return x, y, z
+    version = int.from_bytes(header[5:6], byteorder='little', signed=False)
+    width = int.from_bytes(header[8:10], byteorder='little', signed=False)
+    height = int.from_bytes(header[10:12], byteorder='little', signed=False)
+    return version, width, height
 
 
 class Decoder:
@@ -112,6 +124,7 @@ class Decoder:
 if __name__ == '__main__':
     BLACK = Pixel(0, 0, 0)
     WHITE = Pixel(0xFF, 0xFF, 0xFF)
-    image = Image(2, 2, [BLACK, WHITE, BLACK, WHITE])
-    x = Decoder.load_from('./file.ulbmp')
-    Encoder(x).save_to('file.ulbmp', 2)
+    image = Image(300, 1, [BLACK]*300)
+    # x = Decoder.load_from('./file.ulbmp')
+    Encoder(image, 2).save_to('file.ulbmp')
+    x = Decoder.load_from('file.ulbmp')
